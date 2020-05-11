c50fc4ea83474cb161ba019755ef02b63b6bf738
Anna Balkovskaya <anna.balkovskaya@gmail.com>
2020-05-11 17:29:20 +0300

    Remove file

--- js/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.header__burger').addEventListener('click', (event) => {
-    if(!event.target.classList.contains('header__burger-nav')) {
-        event.currentTarget.classList.toggle('open');
-    }
-});
