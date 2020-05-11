d4665fd69e327dbb90fb60e7ce3b1989ad4485e4
Anna <54270425+Balkanna@users.noreply.github.com>
2020-05-11 17:43:47 +0300

    Refactoring : change burger-menu behavior

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-let burgerBtn = document.getElementById('header__burger-button'),
-    burgerNav = document.getElementsByClassName('header__burger-nav')[0],
-    headerLogo = document.getElementsByClassName('header__logo')[0],
-    overlay = document.getElementsByClassName('header__burger-overlay')[0];
-
-burgerBtn.addEventListener('click', showBurger);
-overlay.addEventListener('click', showBurger);
-
-function showBurger() {
-    headerLogo.style.display === 'none' ? headerLogo.style.display = 'initial' : headerLogo.style.display = 'none';
-    burgerBtn.classList.toggle('burger-menu__active');
-    burgerNav.classList.toggle('burger-nav__active');
-    overlay.classList.toggle('overlay-active');
-}
-
+document.querySelector('.header__burger').addEventListener('click', (event) => {
+    if(!event.target.classList.contains('header__burger-nav')) {
+        event.currentTarget.classList.toggle('open');
+    }
+});
